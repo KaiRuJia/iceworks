5acc7b5b4649f258b8a62ae50cba5cffcab0e0b5
贾凯茹 <jiakairu@jiakairudeMacBook-Pro.local>
2020-12-22 23:50:57 +0800

    feat: formily

--- src/pages/Analysis/index.tsx
+++ src/pages/Analysis/index.tsx
@@ -1,54 +1,104 @@
 import React from 'react';
-import { ResponsiveGrid } from '@alifd/next';
-import PageHeader from '@/components/PageHeader';
-import FusionCardBarChart from './components/FusionCardBarChart';
-import FusionCardAreaChart from './components/FusionCardAreaChart';
-import FusionCardTypebarChart from './components/FusionCardTypebarChart';
-import FusionCardLineChart from './components/FusionCardLineChart';
-import FusionCardRankChart from './components/FusionCardRankChart';
-import FusionCardPieChart from './components/FusionCardPieChart';
-import FusionCardGroupBarChart from './components/FusionCardGroupBarChart';
-import { protectPhone } from '@/utils';
-const { Cell } = ResponsiveGrid;
+import {
+  Input,
+  DatePicker,
+} from '@alifd/next';
+import {
+  SchemaForm,
+  FormButtonGroup,
+  Submit,
+  Reset
+} from '@formily/next'
+
 
 const Analysis = () => {
+  const schema = {
+    "type": "object",
+    "properties": {
+      "NO_NAME_FIELD_$1": {
+        "key": "NO_NAME_FIELD_$1",
+        "type": "object",
+        "name": "NO_NAME_FIELD_$1",
+        "x-component": "mega-layout",
+        "x-component-props": {
+          "grip": true,
+          "columns": 2,
+        },
+        "properties": {
+          "NO_NAME_FIELD_$3": {
+            "key": "NO_NAME_FIELD_$3",
+            "type": "object",
+            "name": "NO_NAME_FIELD_$3",
+            "x-component": "mega-layout",
+            "x-component-props": {
+              "labelCol": 4,
+              "labelAlign": "left"
+            },
+            "properties": {
+              "alignRight": {
+                "key": "alignRight",
+                "name": "alignRight",
+                "title": "标题",
+                "x-component": "input",
+                "x-linkages": [
+                  {
+                    type: "value:visible",
+                    target: "input",
+                    condition: '{{ $self.value == "123"}}',//当值为123时发生联动
+                    schema: {//控制bbb字段的标题，如果不指定condition，默认会走到该处
+                      title: "这是标题"
+                    },
+                    otherwise: {//条件不满足时控制bbb字段标题
+                      title: ""
+                    }
+                  }
+                ]
+              },
+              "input": {
+                "key": "input",
+                "name": "input",
+                "title": "input",
+                "x-component": "input"
+              }
+            }
+          },
+          "input1": {
+            "key": "input1",
+            "name": "input1",
+            "title": "input1",
+            "x-mega-props" : {
+              "span": '1'
+            },
+            "x-component": "input"
+          },
+          "input_2": {
+            "key": "input_2",
+            "name": "input_2",
+            "x-mega-props" : {
+              "span": '1'
+            },
+            "title": "input1",
+            "x-component": "datepicker"
+          }
+        },
+      },
+    }
+  }
   return (
-    <ResponsiveGrid gap={20}>
-      <Cell colSpan={12}>
-        <PageHeader
-          title="分析页面"
-          breadcrumbs={[{ name: 'Dashboard' }, { name: '分析页面' }]}
-        />
-      </Cell>
-
-      <Cell colSpan={3}>
-        <FusionCardBarChart />
-      </Cell>
-
-      <Cell colSpan={3}>
-        <FusionCardAreaChart />
-      </Cell>
-
-      <Cell colSpan={3}>
-        <FusionCardTypebarChart />
-      </Cell>
-
-      <Cell colSpan={3}>
-        <FusionCardLineChart />
-      </Cell>
-
-      <Cell colSpan={12}>
-        <FusionCardRankChart />
-      </Cell>
-
-      <Cell colSpan={4}>
-        <FusionCardPieChart />
-      </Cell>
-
-      <Cell colSpan={8}>
-        <FusionCardGroupBarChart />
-      </Cell>
-    </ResponsiveGrid>
+    <SchemaForm
+      schema={schema}
+      components={{ DatePicker, Input }}
+      expressionScope={{
+        customTitle: 'this is custom title',
+        customDescription: 'this is custom description',
+        customPlaceholder: 'this is custom placeholder'
+      }}
+    >
+      <FormButtonGroup offset={5}>
+        <Submit>查询</Submit>
+        <Reset>重置</Reset>
+      </FormButtonGroup>
+    </SchemaForm>
   );
 };
 
